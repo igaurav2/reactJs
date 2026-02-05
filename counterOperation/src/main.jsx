bb5c9269584279c35a2315998551751d498bc888
Gaurav Pandey <mwc2024001@iiita.ac.in>
2026-02-05 08:50:54 +0000

    nothing

--- counterOperation/src/main.jsx
+++ counterOperation/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
+import IndexCSS from './IndexCSS.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <IndexCSS/>
   </StrictMode>,
 )
